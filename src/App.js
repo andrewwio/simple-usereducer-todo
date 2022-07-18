6652f618fc5b4bcdb52f42895023d8b4fd21114d
Andrew Kain <hello@andreww.io>
2022-07-18 04:15:26 +0400

    Simple add toggle delete todo app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
-import { useReducer } from 'react';
+/* eslint-disable default-case */
+import { useReducer, useState } from 'react';
+import Todo from './Todo'
 
-const ACTIONS = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement'
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
 }
 
-function reducer(state, action) {
+function reducer(todos, action) {
   switch (action.type) {
-    case ACTIONS.INCREMENT:
-      return { count: state.count + 1 }
-    case ACTIONS.DECREMENT:
-      return { count: state.count - 1 }
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
     default:
-      return state
+      return todos
   }
-  
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('')
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } })
+    setName('')
+  }
 
-  function increment() {
-    dispatch({ type: ACTIONS.INCREMENT })
-  }
-  
-  function decrement() {
-    dispatch({ type: ACTIONS.DECREMENT })
-  }
+  console.log(todos)
 
   return (
     <>
-      <button onClick={decrement}>-</button>
-      <span>{state.count}</span>
-      <button onClick={increment}>+</button>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </form>
+      {todos.map(todo => {
+        return <Todo 
+          key={todo.id} 
+          todo={todo} 
+          dispatch={dispatch}
+        />
+      })}
     </>
   );
 }
 
-export default App;
+export default App; 
